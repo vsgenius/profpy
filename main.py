a4a8a650eb0c2d80d0f5e4e0f917ebc600642f30
Vladimir Belich <belichvladimir@mail.ru>
2022-03-02 09:56:07 +0300

    Hw1

---
+++ main.py
@@ -0,0 +1,10 @@
+from applicataion.db.people import get_employees
+from applicataion.salary import calculate_salary
+from datetime import datetime
+
+
+def main():
+    if __name__ == '__main__':
+        print(datetime.now())
+
+main()
